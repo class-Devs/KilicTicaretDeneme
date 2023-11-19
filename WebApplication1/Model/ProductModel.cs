2a9abbdd313ddf1698cd72bde4736d48127690ad
Davut ARSLAN <63652437+DavutArslan@users.noreply.github.com>
2023-11-19 21:48:09 +0300

    product added and ui design

---
+++ WebApplication1/Model/ProductModel.cs
@@ -0,0 +1,8 @@
+// Models/ProductModel.cs
+public class ProductModel
+{
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public decimal Price { get; set; }
+    public string ImageUrl { get; set; }
+}
--- WebApplication1/Pages/Index.cshtml.cs
+++ WebApplication1/Pages/Index.cshtml.cs
@@ -6,6 +6,7 @@ namespace WebApplication1.Pages;
 public class IndexModel : PageModel
 {
     private readonly ILogger<IndexModel> _logger;
+    public List<ProductModel> Products { get; set; }
 
     public IndexModel(ILogger<IndexModel> logger)
     {
@@ -14,5 +15,17 @@ public class IndexModel : PageModel
 
     public void OnGet()
     {
+        Products = GetSampleProducts();
+    }
+    
+    private List<ProductModel> GetSampleProducts()
+    {
+        // Örnek ürünleri oluşturun (gerçek uygulamada bir veritabanından alınmalıdır)
+        return new List<ProductModel>
+        {
+            new ProductModel { Name = "Ürün 1", Description = "Ürün 1 Açıklaması", Price = 10.99m, ImageUrl = "path-to-product-1.jpg" },
+            new ProductModel { Name = "Ürün 2", Description = "Ürün 2 Açıklaması", Price = 19.99m, ImageUrl = "path-to-product-2.jpg" },
+            // Diğer ürünleri ekleyin
+        };
     }
 }
